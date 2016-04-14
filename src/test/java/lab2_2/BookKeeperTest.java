e97f5f67b86c88a8e3225bce6472b5533927d234
bulikd105 <bulikd105@gmail.com>
2016-04-14 19:16:05 +0200

    Drobne zmiany

---
+++ src/test/java/lab2_2/BookKeeperTest.java
@@ -0,0 +1,14 @@
+package lab2_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookKeeperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/lab2_2/InvoiceTest.java
@@ -0,0 +1,22 @@
+package lab2_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class InvoiceTest {
+
+	@Test
+	public void testGrosAndNetCalculation() 
+	{
+		
+	}
+
+}
