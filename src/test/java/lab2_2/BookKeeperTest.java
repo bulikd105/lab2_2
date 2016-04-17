9bb1fb2d033533307fd527f0e2ed4c284873526d
bulikd105 <bulikd105@gmail.com>
2016-04-17 21:24:28 +0200

    Dodanie obiektow do testowania w tescie bookkeeper

--- src/test/java/lab2_2/BookKeeperTest.java
+++ src/test/java/lab2_2/BookKeeperTest.java
@@ -1,14 +1,26 @@
 package lab2_2;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.BookKeeper;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
 
 public class BookKeeperTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+	public final void testIssuance_grosAndNetCalculation() 
+	{
+		//Deklaracje obiektow do testowania
+		InvoiceFactory invoiceFactory = new InvoiceFactory();
+		BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+		ClientData client = new ClientData(Id.generate(), "New Client");
+		InvoiceRequest request = new InvoiceRequest(client);
+		
+	}	
 }
